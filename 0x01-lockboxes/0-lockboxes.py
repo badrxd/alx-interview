dddd454a57d5b101e2dac5764a41fc77e44abf0a
badrxd <badrxdxd@gmail.com>
2024-01-09 16:29:32 +0100

    task 0. Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,8 @@
+def canUnlockAll(boxes):
+    box = set()
+    for i in range(len(boxes)):
+        for j in range(len(boxes[i])):
+            num = boxes[i][j]
+            if num != i and num not in box and num != 0:
+                box.add(num)
+    return True if len(boxes)-1 == len(box) else False
