38fc6e7a054cad01b0748936e3cfc1f0f9a5c474
badrxd <badrxdxd@gmail.com>
2024-01-25 21:53:11 +0100

    fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,39 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
-import sys
-import re
+"""reads stdin line by line and computes metrics """
+
+from sys import stdin
 
 
-def print_fn(status, File_size):
-    """ print function"""
-
-    print("File size: {}".format(File_size))
-    for key, val in status.items():
-        if val != 0:
-            print("{} {}".format(key, val))
+status_code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+total_file_size = 0
+count = 0
 
 
-ptr = (
-    r'^\d+\.\d+\.\d+\.\d+ - \[.*\] '
-    r'"GET \/projects\/\d+ HTTP\/\d.\d" '
-    r'\d{3} \d+$'
-)
-
-File_size = 0
-lines = 0
-status = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
+def print_fn():
+    """function to print all"""
+    print("File size:", total_file_size)
+    for key, value in status_code.items():
+        if value:
+            print("{}: {}".format(key, value))
 
 
 try:
-    for line in sys.stdin:
-        lines += 1
-        match = re.match(ptr, line)
-
-        data = line.split()
-        if match is not None:
-            status[int(data[-2])] += 1
-
+    for line in stdin:
+        count += 1
+        line = line.split()
         try:
-            File_size += int(data[-1])
-        except Exception as err:
-            pass
-
-        if lines % 10 == 0:
-            print_fn(status, File_size)
-    print_fn(status, File_size)
+            file_size = int(line[-1])
+            total_file_size += file_size
+        except (IndexError, ValueError, TypeError):
+            continue
+        try:
+            code = int(line[-2])
+            if code in status_code.keys():
+                status_code[code] += 1
+        except (IndexError, ValueError, TypeError):
+            continue
+        if count % 10 == 0:
+            print_fn()
+    print_fn()
 except KeyboardInterrupt:
-    print_fn(status, File_size)
+    print_fn()
