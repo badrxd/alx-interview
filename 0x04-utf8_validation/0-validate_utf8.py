b639d90864afec97f6d4bec15cfdd2e1d6ab0030
badrxd <badrxdxd@gmail.com>
2024-02-02 03:14:35 +0100

    task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,7 +3,9 @@
 
 
 def validUTF8(data):
-    '''method that determines if a given data set represents a valid UTF-8 encoding'''
+    '''method that determines if a given data set 
+    represents a valid UTF-8 encoding
+    '''
     ln = len(data)
     bytes = 0
     for i in range(ln):
