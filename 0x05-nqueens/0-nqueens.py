103a06da30b6687b0716e15fb136275fd98b263d
badrxd <badrxdxd@gmail.com>
2024-02-14 03:29:53 +0100

    task 1

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,6 +3,16 @@
 from sys import argv, exit
 
 
+def check():
+    pass
+
+
+def search(level, potison):
+    matrix_size = level
+    default_value = 0
+    board = [[default_value] * matrix_size for _ in range(matrix_size)]
+
+
 def nqueens(argv):
 
     if len(argv) != 2:
@@ -18,7 +28,8 @@ def nqueens(argv):
     if int(argv[1]) < 4:
         print('N must be at least 4')
         exit(1)
-    return []
+    solutions = search(int(argv[1]), 0)
+
 
 if __name__ == "__main__":
     nqueens(argv)
